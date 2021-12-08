4f5d458da916198448b546326476a7aae346cc42
Jerrie Feng <jerrie.feng@mail.utoronto.ca>
2021-12-08 13:35:14 -0500

    update data

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' PDB file of 1bm8
+#'
+#' Contains information on the protein 1bm8 gotten from PDB (Protein Data Bank).
+#'
+#' @source PDB, https://www.rcsb.org/structure/1BM8
+#'
+#' @format List of 8 :
+#' \describe{
+#'  \item{Atom}{List containing information on the atom and standard residues }
+#'  \item{XYZ}{atomic coordinate record containing the X,Y,Z}
+#'  \item{seqres}{List containing the Amino Acid sequence}
+#'  \item{Helix}{List indicating the location and type (right-handed alpha, etc.) of helices}
+#'  \item{Sheet}{List indicating the location, sense (anti-parallel, etc.)
+#'  and registration with respect to the previous strand in the sheet (if any) of each strand in the model}
+#'  \item{Remark}{identifies entries in which a re-refinement has been
+#'  performed using the data from an existing entry.}
+#'  \item{Call}{Referring to language/command used to get this}
+#' }
+#' @examples
+#' \dontrun{
+#'  pdb
+#' }
+"pdb"
+# [END] written by Jerrie Feng
+
+
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(pdbSelectOcclude)
-
-test_check("pdbSelectOcclude")
